cac33715b94ca5976b0f4ff3e897d4a67eaba514
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-08-05 22:51:20 +0200

    useRedirect hook

--- src/App.js
+++ src/App.js
@@ -1,56 +1,31 @@
-import React, { useEffect } from 'react';
-import {
-  BrowserRouter, Route, Routes, useNavigate,
-} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import RedirectTo from './RedirectTo';
 
 const App = () => (
   <div>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<RedirectTo path="home" />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/news" element={<News />} />
+        <Route path="/products" element={<Products />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </div>
 );
 
-const Main = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    let didCancel = false;
-    const goToHomePage = () => navigate('/home');
-    if (!didCancel) {
-      goToHomePage();
-    }
-    return () => {
-      didCancel = true;
-    };
-  }, [navigate]);
-  return (
-    <div>
-      <h1>Welcome Main!</h1>
-    </div>
-  );
-};
-
-const Home = () => {
-  const navigate = useNavigate();
-  const goToNewsPage = () => navigate('/news');
-  return (
-    <div>
-      <h1>Welcome Home!</h1>
-      <p>Routing has been redirected to a Home page in React Router 6.</p>
-      <button type="button" onClick={goToNewsPage}>Go to News page</button>
-    </div>
-  );
-};
-
-const News = () => (
+const Home = () => (
   <div>
-    <h1>Welcome News!</h1>
-    <p>Routing has been redirected to a News page in React Router 6.</p>
+    <h1>Welcome Home!</h1>
+    <p>Routing has been redirected to a selected address in React Router 6.</p>
+  </div>
+);
+
+const Products = () => (
+  <div>
+    <h1>Welcome Products!</h1>
   </div>
 );
 
